0a90acaf6117e0df0472bcfa6c0d717b10a3fb18
Aleksandr Shpak <shpaker@users.noreply.github.com>
2020-09-14 19:21:15 +0300

    minor fix (#5)

---
+++ services_toolkit/__init__.py
@@ -0,0 +1,6 @@
+from services_toolkit.__version__ import (  # noqa
+    __title__,
+    __version__,
+    __description__,
+    __url__,
+)
---
+++ services_toolkit/__version__.py
@@ -0,0 +1,4 @@
+__title__ = 'Avatar Services Toolkit'
+__version__ = '0.1.3'
+__description__ = 'Toolkit for services development for avatar ecosystem'
+__url__ = 'https://github.com/avatar-project/avatar-services-toolkit/'
