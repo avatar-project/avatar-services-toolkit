d0f3e9b382bfa028bfbee3331eb53c35430e035c
Artem Davletov <Tomas-Cooper@mail.ru>
2020-10-28 20:25:14 +0300

    Modify response body

--- services_toolkit/validation/response_tool.py
+++ services_toolkit/validation/response_tool.py
@@ -8,9 +8,9 @@ def create_response(
         status: int = 200, message: str = None, headers: Dict[str, Any] = None, data: Dict[str, Any] = None
 ) -> Tuple[Response, int]:
     response = {
-        status: status,
-        message: message if message else '',
-        headers: headers if headers else {},
-        data: str(data) if data else {},
+        'status': status,
+        'message': message or '',
+        'headers': headers or {},
+        'data': data or {},
     }
     return jsonify(response), status
