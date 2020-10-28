c62f9eecb4e78e7f293d1cdaa295e2addff93384
Artem Davletov <Tomas-Cooper@mail.ru>
2020-10-28 18:25:11 +0300

    Make it works

--- services_toolkit/validation/read_version.py
+++ services_toolkit/validation/read_version.py
@@ -1,12 +1,12 @@
-import os
 from logging import getLogger
+from pathlib import Path
 
 logger = getLogger(__name__)
 
 
-def read_version(path):
-    if not os.path.isfile:
-        return None
+def read_version(path: str) -> str:
+    if not Path(path).exists():
+        return '0.0.0'
 
     with open(path, encoding='utf-8') as file:
         version = file.readline().strip()
