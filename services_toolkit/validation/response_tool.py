fd2d3dbee588f049dc5ad8f0717cae373d73af64
Artem Davletov <Tomas-Cooper@mail.ru>
2020-10-29 11:49:18 +0300

    Fix key in create-response

--- services_toolkit/validation/response_tool.py
+++ services_toolkit/validation/response_tool.py
@@ -8,9 +8,9 @@ def create_response(
         status: int = 200, message: str = None, headers: Dict[str, Any] = None, data: Dict[str, Any] = None
 ) -> Tuple[Response, int]:
     response = {
-        'status': status,
+        'success': status,
         'message': message or '',
+        'result': data or {},
         'headers': headers or {},
-        'data': data or {},
     }
     return jsonify(response), status
